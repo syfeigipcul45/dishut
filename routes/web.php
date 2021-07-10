5886db9a2b349f03e7a96f9037ae4c4e6a6e8a4e
syfeigipcul45 <63193446+syfeigipcul45@users.noreply.github.com>
2021-07-10 13:56:23 +0800

    beranda web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Web\BerandaController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[BerandaController::class, 'index'])->name('beranda');
 
 Auth::routes();
 
